6c7cf7c0ee1c10f3b83def25d45c07f158c791bb
Roman Kovalchuk <deputatroma@gmail.com>
2021-02-26 15:15:53 +0200

    Arhiv-3

--- js/script.js
+++ js/script.js
@@ -19,3 +19,20 @@ menu.addEventListener("click", (event) => {
         event.target.parentNode.classList.add("menu-item--active");
     }
 });
+let clicks_1 = 1397;
+function clickME_1() {
+    clicks_1 += 1;
+    document.getElementById("clicks_1").innerHTML = clicks_1 / 1000;
+}
+
+let clicks_2 = 21038;
+function clickME_2() {
+    clicks_2 += 1;
+    document.getElementById("clicks_2").innerHTML = clicks_2 / 1000;
+}
+
+let clicks_3 = 4391;
+function clickME_3() {
+    clicks_3 += 1;
+    document.getElementById("clicks_3").innerHTML = clicks_3 / 1000;
+}
